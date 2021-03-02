6c21893aaf2aa5bad029bbbc1090cf2b07fa6868
dalessioluca <ldalessi@broadinstitute.org>
2021-03-01 22:08:03 -0500

    activated the clamping

--- src/genus/model_parts.py
+++ src/genus/model_parts.py
@@ -219,9 +219,10 @@ class InferenceAndGeneration(torch.nn.Module):
         # ---------------------------#
         unet_output: UNEToutput = self.unet.forward(imgs_bcwh, verbose=False)
         unet_prob_b1wh = torch.sigmoid(unet_output.logit)
+        unet_output.logit.register_hook(lambda grad: grad.clamp(min=-0.01, max=0.01))
+
         # unet_output.logit.register_hook(lambda grad: print("grad before clipping:",
         #                                                   torch.min(grad), torch.mean(grad), torch.max(grad)))
-        # unet_output.logit.register_hook(lambda grad: grad.clamp(min=-10, max=10))
         # unet_output.logit.register_hook(lambda grad: print("grad after clipping:",
         #                                                   torch.min(grad), torch.mean(grad), torch.max(grad)))
 
