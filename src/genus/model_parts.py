a981253149bdaf0b3f4a78d9339508dcd106d165
dalessioluca <ldalessi@broadinstitute.org>
2021-03-04 14:08:20 -0500

    ncell_av = (prob_mbk > 0.5)

--- src/genus/model_parts.py
+++ src/genus/model_parts.py
@@ -446,7 +446,7 @@ class InferenceAndGeneration(torch.nn.Module):
             # NCELL_AV
             self.geco_rawlambda_ncell.data.clamp_(min=self.geco_rawlambda_ncell_min,
                                                   max=self.geco_rawlambda_ncell_max)
-            ncell_av = prob_mbk.sum(dim=-1).float().mean()
+            ncell_av = (prob_mbk > 0.5).sum(dim=-1).float().mean()
             # ncell_av = c_detached_mbk.sum(dim=-1).float().mean()
             lambda_ncell = linear_quadratic_activation(self.geco_rawlambda_ncell.data) * \
                            torch.sign(ncell_av - self.geco_target_ncell_min)
