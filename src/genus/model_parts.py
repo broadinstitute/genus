wrong shape

src/genus/model_parts.py
@@ -373,7 +373,7 @@ class InferenceAndGeneration(torch.nn.Module):
             # 4. From logit to binarized configuration of having an object at a certain location
             if generate_synthetic_data:
                 # sample from dpp prior
-                c_grid_before_nms = self.grid_dpp.sample(size=unet_prob_b1wh.size()).unsqueeze(dim=0)
+                c_grid_before_nms = self.grid_dpp.sample(size=unet_prob_b1wh.size())
             else:
                 # sample from posterior
                 c_grid_before_nms = (torch.rand_like(unet_prob_b1wh) < unet_prob_b1wh) if noisy_sampling \

src/genus/util_ml.py
@@ -464,10 +464,12 @@ class Grid_DPP(torch.nn.Module):
     def sample(self, size: torch.Size):
         """
         Draw a random sample of size torch.Size according to the current values of length_scale, weight.
-        Note that size must be at least 2D.
+        The last two dimensions are treated as spatial coordinates.
+        All other dimension are assumed independent.
         The samples are not differentiable.
         """
-        assert len(size) >= 2
+        assert len(size) >= 2, "the spatial dimension must be at least 2. Instead I found {0}".format(len(size))
+
         current_figerprint = (self.similiraty_kernel.length_scale_value.data.item(),
                               self.similiraty_kernel.weight_value.data.item(),
                               size[-2], size[-1])

src/main_mnist.py
@@ -20,7 +20,7 @@ torch.manual_seed(0)
 numpy.random.seed(0)
 
 config = load_yaml_as_dict("./config.yaml")
-exp: neptune.run.Run = neptune.init(project='dalessioluca/genus-new',
+exp: neptune.run.Run = neptune.init(project=project=config["neptune_project"],
                                     source_files=["main*.py", "*/*.py", "config.yaml"],
                                     mode="async",
                                     capture_stdout=True,